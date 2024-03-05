448820fb565358c80ac28834f431c9f5ab03e68e
MARIAca02 <159548894+MARIAca02@users.noreply.github.com>
2024-03-05 10:30:51 -0500

    Add files via upload

--- comercial_foods.sql
+++ comercial_foods.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-02-2024 a las 16:02:39
+-- Tiempo de generación: 05-03-2024 a las 16:30:25
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -48,6 +48,173 @@ CREATE TABLE `categoria` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `cod_cliente` int(11) NOT NULL,
+  `nombre1` varchar(25) NOT NULL,
+  `nombre2` varchar(25) NOT NULL,
+  `apellido1` varchar(25) NOT NULL,
+  `apellido2` varchar(25) NOT NULL,
+  `tipo_documento` enum('cc','ce','nit','rut') DEFAULT NULL,
+  `no_documento` varchar(25) NOT NULL,
+  `sexo` enum('masculino','femenino') DEFAULT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `ciudad` varchar(20) NOT NULL,
+  `fecha_ingreso` date DEFAULT curdate(),
+  `edad` int(11) NOT NULL,
+  `telefono` varchar(25) NOT NULL,
+  `estado_civil` enum('soltero','casado','divorsiado','union libre','viudo') DEFAULT NULL,
+  `tipo_cliente` enum('detallista','mayorista','empresarial') DEFAULT NULL,
+  `activo` varchar(4) NOT NULL DEFAULT 'A',
+  `empleado_cod` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contratista`
+--
+
+CREATE TABLE `contratista` (
+  `cod_contratista` int(11) NOT NULL,
+  `nombre1` varchar(25) NOT NULL,
+  `nombre2` varchar(25) NOT NULL,
+  `apellido1` varchar(25) NOT NULL,
+  `apellido2` varchar(25) NOT NULL,
+  `tipo_documento` enum('cc','ce','nit','rut') DEFAULT NULL,
+  `no_documento` varchar(25) NOT NULL,
+  `sexo` enum('masculino','femenino') DEFAULT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `telefono` varchar(25) NOT NULL,
+  `camion` enum('camineta','furgon','camion') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `despacho`
+--
+
+CREATE TABLE `despacho` (
+  `cod_despacho` int(11) NOT NULL,
+  `contratista_cod` int(11) NOT NULL,
+  `factura_cod` int(11) NOT NULL,
+  `fecha_recibo` date NOT NULL,
+  `fecha_entrega` date NOT NULL,
+  `valor_flete` decimal(10,0) NOT NULL DEFAULT 0,
+  `entregado` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `cod_empleado` int(11) NOT NULL,
+  `nombre1` varchar(25) NOT NULL,
+  `nombre2` varchar(25) NOT NULL,
+  `apellido1` varchar(25) NOT NULL,
+  `apellido2` varchar(25) NOT NULL,
+  `tipo_documento` enum('cc','ce','nit','rut') DEFAULT NULL,
+  `no_documento` varchar(25) NOT NULL,
+  `sexo` enum('masculino','femenino') DEFAULT NULL,
+  `direcion` varchar(50) NOT NULL,
+  `ciudad` varchar(20) NOT NULL,
+  `edad` int(11) NOT NULL CHECK (`edad` >= 18),
+  `telefono` varchar(25) NOT NULL,
+  `fecha_ingreso` date DEFAULT curdate(),
+  `estado_civil` enum('soltero','casado','divorciado','union libre','viudo') DEFAULT NULL,
+  `cargo_cod` int(11) NOT NULL,
+  `nivel_estudios` enum('primaria','bachillerato','tecnico','tecnologo','profesional','otro') DEFAULT NULL,
+  `eps` enum('sanitas','sura','capital salud','nueva  eps','compensar','famisanar','aliansalud') DEFAULT NULL,
+  `pensiones` enum('colfondos','proteccion','porvenir','skadia') DEFAULT NULL,
+  `cesantias` enum('fna','porvenir','colfondos','proteccion') DEFAULT NULL,
+  `banco` enum('BBVA','davivienda','bancolombia','caja social','popular','las villas') DEFAULT NULL,
+  `activo` varchar(4) NOT NULL DEFAULT 'A'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura_cabeza`
+--
+
+CREATE TABLE `factura_cabeza` (
+  `cod_factura` int(11) NOT NULL,
+  `fecha_factura` date NOT NULL,
+  `cliente_cod` int(11) NOT NULL,
+  `empleado_cod` int(11) NOT NULL,
+  `forma_pago` enum('efectivo','nequi','credito') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura_detalle`
+--
+
+CREATE TABLE `factura_detalle` (
+  `cod_facdetalle` int(11) NOT NULL,
+  `factura_cod` int(11) NOT NULL,
+  `producto_cod` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `valor_venta` decimal(10,0) NOT NULL DEFAULT 0,
+  `subtotal` decimal(10,0) GENERATED ALWAYS AS (`valor_venta` * `cantidad`) VIRTUAL,
+  `descuento` decimal(10,2) NOT NULL DEFAULT 0.00,
+  `neto` decimal(10,0) GENERATED ALWAYS AS (`subtotal` * (1 - `descuento`)) VIRTUAL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `nomina`
+--
+
+CREATE TABLE `nomina` (
+  `cod_nomina` int(11) NOT NULL,
+  `fecha_nomina` date NOT NULL,
+  `empleado_cod` int(11) NOT NULL,
+  `salario_base` decimal(10,0) NOT NULL DEFAULT 0,
+  `dias_trabajados` int(11) NOT NULL,
+  `salario` decimal(10,0) NOT NULL,
+  `auxilio_transporte` decimal(10,0) NOT NULL DEFAULT 0,
+  `nro_hrecargo` int(11) NOT NULL DEFAULT 0,
+  `recargo_noche` decimal(10,0) NOT NULL,
+  `comisiones_otros` decimal(10,0) NOT NULL DEFAULT 0,
+  `total_devengado` decimal(10,0) NOT NULL,
+  `salut` decimal(10,0) NOT NULL,
+  `pension` decimal(10,0) NOT NULL,
+  `prestamos_otros` decimal(10,0) NOT NULL DEFAULT 0,
+  `tota_deducido` decimal(10,0) NOT NULL,
+  `neto_pagar` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `cod_producto` int(11) NOT NULL,
+  `descripcon` varchar(50) NOT NULL,
+  `valor_compra` decimal(10,0) NOT NULL DEFAULT 0,
+  `valor_venta` decimal(10,0) NOT NULL DEFAULT 0,
+  `existencia` bigint(20) NOT NULL CHECK (`existencia` >= 0),
+  `nro_lote` int(11) NOT NULL,
+  `fecha_fabricacion` date NOT NULL,
+  `fecha_vencimiento` date NOT NULL,
+  `categor_cod` int(11) NOT NULL,
+  `proveedor_cod` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `proveedor`
 --
 
@@ -83,6 +250,70 @@ ALTER TABLE `categoria`
   ADD KEY `nombre_categoria` (`nombre_categoria`);
 
 --
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`cod_cliente`),
+  ADD KEY `nombre1` (`nombre1`,`apellido1`),
+  ADD KEY `empleado_cod` (`empleado_cod`);
+
+--
+-- Indices de la tabla `contratista`
+--
+ALTER TABLE `contratista`
+  ADD PRIMARY KEY (`cod_contratista`);
+
+--
+-- Indices de la tabla `despacho`
+--
+ALTER TABLE `despacho`
+  ADD PRIMARY KEY (`cod_despacho`),
+  ADD KEY `fecha_entrega` (`fecha_entrega`,`entregado`),
+  ADD KEY `contratista_cod` (`contratista_cod`),
+  ADD KEY `factura_cod` (`factura_cod`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`cod_empleado`),
+  ADD KEY `nombre1` (`nombre1`),
+  ADD KEY `cargo_cod` (`cargo_cod`);
+
+--
+-- Indices de la tabla `factura_cabeza`
+--
+ALTER TABLE `factura_cabeza`
+  ADD PRIMARY KEY (`cod_factura`),
+  ADD KEY `cliente_cod` (`cliente_cod`),
+  ADD KEY `empleado_cod` (`empleado_cod`);
+
+--
+-- Indices de la tabla `factura_detalle`
+--
+ALTER TABLE `factura_detalle`
+  ADD PRIMARY KEY (`cod_facdetalle`),
+  ADD KEY `factura_cod` (`factura_cod`),
+  ADD KEY `producto_cod` (`producto_cod`);
+
+--
+-- Indices de la tabla `nomina`
+--
+ALTER TABLE `nomina`
+  ADD PRIMARY KEY (`cod_nomina`),
+  ADD KEY `fecha_nomina` (`fecha_nomina`),
+  ADD KEY `empleado_cod` (`empleado_cod`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`cod_producto`),
+  ADD KEY `descripcon` (`descripcon`),
+  ADD KEY `categor_cod` (`categor_cod`),
+  ADD KEY `proveedor_cod` (`proveedor_cod`);
+
+--
 -- Indices de la tabla `proveedor`
 --
 ALTER TABLE `proveedor`
@@ -106,10 +337,108 @@ ALTER TABLE `categoria`
   MODIFY `cod_categoria` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `cod_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `contratista`
+--
+ALTER TABLE `contratista`
+  MODIFY `cod_contratista` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `despacho`
+--
+ALTER TABLE `despacho`
+  MODIFY `cod_despacho` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `cod_empleado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura_cabeza`
+--
+ALTER TABLE `factura_cabeza`
+  MODIFY `cod_factura` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura_detalle`
+--
+ALTER TABLE `factura_detalle`
+  MODIFY `cod_facdetalle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `nomina`
+--
+ALTER TABLE `nomina`
+  MODIFY `cod_nomina` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `cod_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `proveedor`
 --
 ALTER TABLE `proveedor`
   MODIFY `cod_proveedor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `cliente_ibfk_1` FOREIGN KEY (`empleado_cod`) REFERENCES `empleado` (`cod_empleado`);
+
+--
+-- Filtros para la tabla `despacho`
+--
+ALTER TABLE `despacho`
+  ADD CONSTRAINT `despacho_ibfk_1` FOREIGN KEY (`contratista_cod`) REFERENCES `contratista` (`cod_contratista`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `despacho_ibfk_2` FOREIGN KEY (`factura_cod`) REFERENCES `factura_cabeza` (`cod_factura`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`cargo_cod`) REFERENCES `cargo` (`cod_cargo`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `factura_cabeza`
+--
+ALTER TABLE `factura_cabeza`
+  ADD CONSTRAINT `factura_cabeza_ibfk_1` FOREIGN KEY (`cliente_cod`) REFERENCES `cliente` (`cod_cliente`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `factura_cabeza_ibfk_2` FOREIGN KEY (`empleado_cod`) REFERENCES `empleado` (`cod_empleado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `factura_detalle`
+--
+ALTER TABLE `factura_detalle`
+  ADD CONSTRAINT `factura_detalle_ibfk_1` FOREIGN KEY (`factura_cod`) REFERENCES `factura_cabeza` (`cod_factura`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `factura_detalle_ibfk_2` FOREIGN KEY (`producto_cod`) REFERENCES `productos` (`cod_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `nomina`
+--
+ALTER TABLE `nomina`
+  ADD CONSTRAINT `nomina_ibfk_1` FOREIGN KEY (`empleado_cod`) REFERENCES `empleado` (`cod_empleado`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos_ibfk_1` FOREIGN KEY (`categor_cod`) REFERENCES `categoria` (`cod_categoria`),
+  ADD CONSTRAINT `productos_ibfk_2` FOREIGN KEY (`proveedor_cod`) REFERENCES `proveedor` (`cod_proveedor`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
